228a64d3f8430bd8475f759f2dc8c1e59505dc1d
Mohammad Murad Esawi <146820485+mohammadEsawi@users.noreply.github.com>
2024-09-21 16:03:33 +0300

    show data in table

---
+++ CRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,16 @@
+﻿using CRUD.data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace CRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var employees = context.employees.ToList();
+            return View("Index", employees);
+        }
+    }
+}
