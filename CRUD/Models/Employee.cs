49b06c946bf88105e0cec583a39675634cc3b070
Mohammad Murad Esawi <146820485+mohammadEsawi@users.noreply.github.com>
2024-09-21 15:24:49 +0300

    Add project files.

---
+++ CRUD/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [DataType("varchar")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string Email { get; set; } = null!;
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ CRUD/data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=. ;Database=CrudDATA; Trusted_Connection=True;trustservercertificate=true");
+        }
+
+       public  DbSet<Employee> employees { get; set; } 
+    }
+}
